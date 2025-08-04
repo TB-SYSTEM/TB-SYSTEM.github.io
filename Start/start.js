9bab7d23c7812c263ef9f3bcfdb8f95734f96081
‘TB-SYSTEM’ <3905104952@qq.com>
2025-08-04 17:32:11 +0800

    壁纸更新

--- Start/start.js
+++ Start/start.js
@@ -1,5 +1,5 @@
 ﻿const projects = [
-  { id: 'project-1', title: '项目一',  desc: '简短描述一句话' },
+  { id: 'project-1', title: '2048',  desc: '经典2048，awa' },
   { id: 'project-2', title: '项目二',  desc: '简短描述一句话' },
   { id: 'project-3', title: '项目三',  desc: '简短描述一句话' },
   { id: 'project-4', title: '项目四',  desc: '简短描述一句话' }
